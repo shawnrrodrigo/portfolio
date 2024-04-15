8eb57824fc0431f4a5d50d2192fbe54ff916d2a0
shawnrrodrigo <2019cs145@stu.ucsc.cmb.ac.lk>
2024-04-15 22:58:54 +0530

    Responsive fix

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -25,8 +25,8 @@ export default function About() {
             <img
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
               alt="JavaScript"
-              width="40"
-              height="40"
+              width="25"
+              height="25"
             />
           </a>
          
@@ -34,24 +34,24 @@ export default function About() {
             <img
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg"
               alt="C"
-              width="40"
-              height="40"
+              width="25"
+              height="25"
             />
           </a>
           <a href="https://www.w3.org/html/">
             <img
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
               alt="HTML5"
-              width="40"
-              height="40"
+              width="25"
+              height="25"
             />
           </a>
           <a href="https://www.w3schools.com/css/">
             <img
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
               alt="CSS3"
-              width="40"
-              height="40"
+              width="25"
+              height="25"
             />
           </a>
          
@@ -60,8 +60,8 @@ export default function About() {
             <img
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg"
               alt="Java"
-              width="40"
-              height="40"
+              width="25"
+              height="25"
             />
           </a>
           
@@ -69,8 +69,8 @@ export default function About() {
             <img
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg"
               alt="Bootstrap"
-              width="40"
-              height="40"
+              width="25"
+              height="25"
               style={{ filter: 'brightness(0) invert(1)' }}
             />
           </a>
@@ -78,40 +78,40 @@ export default function About() {
             <img
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/vuejs/vuejs-original-wordmark.svg"
               alt="Vue.js"
-              width="40"
-              height="40"
+              width="25"
+              height="25"
             />
           </a>
           <a href="https://reactjs.org/">
             <img
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
               alt="React.js"
-              width="40"
-              height="40"
+              width="25"
+              height="25"
             />
           </a>
           <a href="https://jquery.com/">
             <img
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/jquery/jquery-original-wordmark.svg"
               alt="jQuery"
-              width="40"
-              height="40"
+              width="25"
+              height="25"
             />
           </a>
           <a href="https://nodejs.org/">
             <img
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg"
               alt="Node.js"
-              width="40"
-              height="40"
+              width="25"
+              height="25"
             />
           </a>
           <a href="https://expressjs.com/">
             <img
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg"
               alt="Express.js"
-              width="40"
-              height="40"
+              width="25"
+              height="25"
               style={{ filter: 'brightness(0) invert(1)' }}
             />
           </a>
@@ -119,48 +119,48 @@ export default function About() {
             <img
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/spring/spring-original-wordmark.svg"
               alt="Spring"
-              width="40"
-              height="40"
+              width="25"
+              height="25"
             />
           </a>
           <a href="https://flutter.dev/" >
             <img
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/flutter/flutter-original.svg"
               alt="Flutter"
-              width="40"
-              height="40"
+              width="25"
+              height="25"
             />
           </a>
           <a href="https://aws.amazon.com/">
             <img
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/amazonwebservices/amazonwebservices-original-wordmark.svg"
               alt="AWS"
-              width="40"
-              height="40"
+              width="25"
+              height="25"
             />
           </a>
           <a href="https://www.mongodb.com/">
             <img
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg"
               alt="MongoDB"
-              width="40"
-              height="40"
+              width="25"
+              height="25"
             />
           </a>
           <a href="https://firebase.google.com/">
             <img
               src="https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg"
               alt="Firebase"
-              width="40"
-              height="40"
+              width="25"
+              height="25"
             />
           </a>
           <a href="https://www.mysql.com/">
             <img
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg"
               alt="MySQL"
-              width="40"
-              height="40"
+              width="25"
+              height="25"
             />
           </a>
           
